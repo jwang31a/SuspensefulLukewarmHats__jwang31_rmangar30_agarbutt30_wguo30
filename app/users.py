536be1bf213e7efeef8156eb6ab816428d2ad0c2
RavindraM23 <rmangar30@stuy.edu>
2022-12-15 13:04:50 -0500

    DATABASE USERS.PY

--- app/users.py
+++ app/users.py
@@ -26,12 +26,21 @@ def add_accounts(username, movies):
   #c = db.cursor()
   current = get_accounts(username)
   for item in movies:
+    if item in current:
+      continue
     current.append(item)
-  print(f"INSERT: {current}")
-  #try:
-  c.execute("UPDATE accounts WHERE username(?) SET movies(?);")
-  #except:
-  #  c.execute("INSERT INTO accounts VALUES(?, ?);", [username, "_".join(current)])
+  #print(f"INSERT: {current}")
+  c.execute("UPDATE accounts SET movies = ? WHERE username = ?", ["_".join(current), str(username)])
+  db.commit()
+  c.execute("INSERT INTO accounts VALUES(?, ?);", [username, "_".join(current)])
+
+def remove_accounts(username, movies):
+  current = get_accounts(username)
+  for item in movies:
+    current.remove(item)
+  #print(f"REMOVE: {current}")
+  c.execute("UPDATE accounts SET movies = ? WHERE username = ?", ["_".join(current), str(username)])
+  db.commit()
 
 def get_accounts(username):
     #DB_FILE="accounts.db"
@@ -42,7 +51,7 @@ def get_accounts(username):
     #print(response)
     for account in response:
         if account[0] == username:
-            print(f"GET {account[1]}")
+            #print(f"GET {account[1]}")
             return account[1].split("_")
     return []
 
@@ -53,3 +62,4 @@ add_accounts("111", "111")
 add_accounts("222", "222")
 print(get_accounts("dudu", "dudu"))
 '''
+#verify_accounts()
