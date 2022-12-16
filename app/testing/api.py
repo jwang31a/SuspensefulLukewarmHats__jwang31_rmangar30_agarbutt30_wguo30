4bea4cf276a33d14bae262b5e5da84268518dce7
Jun Hong Wang <jwang31@stuy.edu>
2022-12-16 13:18:54 -0500

    more testing'

--- app/testing/api.py
+++ app/testing/api.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 #opens key for omdbapi, turns it into string
 omdb = open("../keys/key_omdapi.txt").read()
 #print(omdb)
+watchmode = open("../keys/key_watchmode.txt").read()
 
 app = Flask(__name__)
 
@@ -13,10 +14,29 @@ def req():
     url = f"http://www.omdbapi.com/?apikey={omdb}&t=Infinity+War"
     #print(url)
     info = json.load(urllib.request.urlopen(url))
+    #print(info)
     i = info["Poster"]
     print(i)
+    with urllib.request.urlopen(f"https://api.watchmode.com/v1/search/?apiKey={watchmode}&search_field=name&search_value=Breaking+Bad") as url:
+        data = json.loads(url.read().decode())
+        print(data)
     #make sure in jinja templating the brackets are {{ }}
     return render_template("testing.html", imageurl=i)
+
+"""
+from serpapi import GoogleSearch
+
+params = {
+  "q": "McDonald's",
+  "location": "austin, texas, united states",
+  "tbm": "lcl",
+  "api_key": "secret_api_key"
+}
+
+search = GoogleSearch(params)
+results = search.get_dict()
+local_results = results["local_results"]
+"""
 
 if __name__ == "__main__":
     app.debug = True
