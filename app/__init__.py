563cfa5115da6cb4934ac6afcfc8b2bf9bfe1502
RavindraM23 <rmangar30@stuy.edu>
2022-12-20 13:16:15 -0500

    WATCHMODE API MERGE

--- app/__init__.py
+++ app/__init__.py
@@ -56,8 +56,14 @@ def theatres():
 def anime():
 
     key = 'YaI3KDpNull6LCiQp3skU1ybBUQCEUD0bC6daLAW'
-    response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_asc&limit=5')
-    response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&release_date_start=20220101&limit=5')
+
+    #ANIME
+    response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_asc&limit=5&genres=33')
+    response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&release_date_start=20220101&limit=5&genres=33')
+    
+    #COMEDY
+    response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_asc&limit=5&genres=4')
+    response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&release_date_start=20220101&limit=5&genres=4')
 
     return render_template("anime.html")
 
--- app/test.py
+++ app/test.py
@@ -13,10 +13,10 @@ import requests
 #print(response.text)
 
 key = 'YaI3KDpNull6LCiQp3skU1ybBUQCEUD0bC6daLAW'
-#response1 = requests.get(f'https://api.watchmode.com/v1/genres/?apiKey={key}')
-response1 = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_asc&limit=5&genres=33')
-response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_desc&limit=5&genres=33')
+response1 = requests.get(f'https://api.watchmode.com/v1/genres/?apiKey={key}')
+#response1 = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_asc&limit=5&genres=33')
+#response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_desc&limit=5&genres=33')
 #print(response)
-print(response.text)
-print("\n\n")
+#print(response.text)
+#print("\n\n")
 print(response1.text)
