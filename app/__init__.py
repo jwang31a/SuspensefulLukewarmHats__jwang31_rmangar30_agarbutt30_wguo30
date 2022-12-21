c328977fd8bcefe9f46b5987dd6ffb0300c93b42
Your Name <you@example.com>
2022-12-20 20:38:04 -0500

    small changes

--- app/__init__.py
+++ app/__init__.py
@@ -54,8 +54,7 @@ def theatres():
 
 @app.route("/anime", methods = ["GET", "POST"])
 def anime():
-
-    key = 'YaI3KDpNull6LCiQp3skU1ybBUQCEUD0bC6daLAW'
+    key = open("../app/keys/key_anime.txt", "r").read()
 
     #ANIME
     response = requests.get(f'https://api.watchmode.com/v1/list-titles/?apiKey={key}&sort_by=popularity_asc&limit=5&genres=33')
